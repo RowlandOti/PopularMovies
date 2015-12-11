ffa69e458084602f690effb2ac606fe19f44496a
RowlandOti <rowlandotienoo@gmail.com>
2015-12-11 23:35:23 +0300

    chore: added the LICENCE in individual files

--- app/src/main/java/com/rowland/movies/rest/services/IMDbEndPointsApi.java
+++ app/src/main/java/com/rowland/movies/rest/services/IMDbEndPointsApi.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2015 Oti Rowland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
 package com.rowland.movies.rest.services;
 
 import com.rowland.movies.rest.data.MoviesData;
 import com.rowland.movies.rest.data.ReviewsData;
 import com.rowland.movies.rest.data.TrailersData;
-import com.rowland.movies.rest.pojos.Movies;
 
 import retrofit.Call;
 import retrofit.http.GET;
@@ -19,11 +36,11 @@ public interface IMDbEndPointsApi {
      * to encode details about the parameters and request method.
      *
      * Created by Rowland  on 12/11/2015.
-     * */
+     **/
 
     // Will load our movies
     @GET("/3/discover/movie")
-    Call<Movies> loadMoviesData(@Query("apiKey") String apiKey, @Query("sortBy") String sortBy);
+    Call<MoviesData> loadMoviesData(@Query("apiKey") String apiKey, @Query("sortBy") String sortBy);
 
     // Will load the movie's trailer videos
     @GET("/3/movie/{id}/videos")
