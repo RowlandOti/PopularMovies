f7568dd0ac041719e431b6e52fccccc9df9a30bb
RowlandOti <rowlandotienoo@gmail.com>
2015-12-21 12:02:36 +0300

    feat: now using TrailersCallback class in TrailersLoader

---
+++ app/src/main/java/com/rowland/movies/asynctaskloaders/callbacks/TrailersCallBack.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015 Oti Rowland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.rowland.movies.asynctaskloaders.callbacks;
+
+import android.util.Log;
+
+import com.rowland.movies.BuildConfig;
+import com.rowland.movies.rest.collections.TrailersCollection;
+import com.rowland.movies.rest.pojos.RestError;
+import com.rowland.movies.rest.pojos.Trailers;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+/**
+ * Created by Oti Rowland on 12/21/2015.
+ */
+public abstract class TrailersCallBack implements Callback<TrailersCollection> {
+
+    // The class Log identifier
+    private static final String LOG_TAG = MoviesCallBack.class.getSimpleName();
+    // The list of movies our loader returns
+    private List<Trailers> trailersList;
+
+    @Override
+    public void onResponse(Response<TrailersCollection> response, Retrofit retrofit) {
+
+        if (response.isSuccess() && response.errorBody() == null) {
+            // movies available
+            trailersList = response.body().getResults();
+
+            for (Trailers trailer : trailersList) {
+                // Save movies in the database
+                trailer.save();
+                // Check wether we are in debug mode
+                if (BuildConfig.IS_DEBUG_MODE) {
+                    Log.d(LOG_TAG, "Trailer " + trailer.getName());
+                    Log.d(LOG_TAG, "Trailer " + trailer.getSite());
+                }
+            }
+        } else {
+
+            try {
+                RestError restError = (RestError) retrofit
+                        .responseConverter(RestError.class, RestError.class.getAnnotations())
+                        .convert(response.errorBody());
+                if (BuildConfig.IS_DEBUG_MODE) {
+                    // we got an error message - Do error handling here
+                    Log.d(LOG_TAG, restError.getStrMesage());
+                    //For getting error code. Code is integer value like 200,404 etc
+                    Log.d(LOG_TAG, String.valueOf(restError.getCode()));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+        // Inform user of failure due to no network e.t.c
+        Log.d(LOG_TAG, t.getMessage());
+    }
+    // Getter method for moviesList
+    public List<Trailers> getTrailersList() {
+
+        return this.trailersList;
+    }
+    // A handy method to retrieve the list from the callback
+    // Implement this method to gain access
+    public abstract void retrieveTrailersList();
+}
