229e02f144258be3c4587f2ad67cd2a0d43dde50
RowlandOti <rowlandotienoo@gmail.com>
2015-12-28 20:10:04 +0300

    chore: nest the empty TextView inside the SwipableLayout

--- app/src/main/java/com/rowland/movies/ui/activities/DetailActivity.java
+++ app/src/main/java/com/rowland/movies/ui/activities/DetailActivity.java
@@ -26,24 +26,13 @@ import android.view.View;
 
 import com.rowland.movies.R;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends BaseToolBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
