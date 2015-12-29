50dee4ef25221eb88a43d87fae2cb88bdda0efcc
RowlandOti <rowlandotienoo@gmail.com>
2015-12-29 08:30:16 +0300

    refactor: update DetailActivity

--- app/src/main/java/com/rowland/movies/ui/activities/DetailActivity.java
+++ app/src/main/java/com/rowland/movies/ui/activities/DetailActivity.java
@@ -18,17 +18,20 @@
 package com.rowland.movies.ui.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.rowland.movies.R;
+import com.rowland.movies.ui.fragments.DetailFragment;
 
 import butterknife.ButterKnife;
 
 public class DetailActivity extends BaseToolBarActivity {
+
+    // Logging Identifier for class
+    private final String LOG_TAG = DetailActivity.class.getSimpleName();
+    // The DetailFragment
+    private DetailFragment detailFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +41,43 @@ public class DetailActivity extends BaseToolBarActivity {
         // Inject all the views
         ButterKnife.bind(this);
         // Setup the inc_toolbar
-        setToolbar(true, false, R.drawable.ic_logo_48px);
+        setToolbar(false, false, R.drawable.ic_logo_48px);
+        // Check that the activity is using the layout with the fragment_container id
+        if (findViewById(R.id.fragment_container) != null)
+        {
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null)
+            {
+                return;
+            }
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+            else
+            {
+                int idKey = getIntent().getIntExtra(DetailFragment.MOVIE_ID_KEY, 0);
+                // Create a Bundle object
+                Bundle args = new Bundle();
+                // Set arguments on Bundle
+                args.putInt(DetailFragment.MOVIE_ID_KEY, idKey);
+                // Pass bundle to the fragment
+                showDetailFragment(args);
+            }
+        }
 
     }
+    // Insert the DetailFragment
+    private void showDetailFragment(Bundle args) {
+        FragmentManager fm = getSupportFragmentManager();
 
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (detailFragment == null) {
+            detailFragment = DetailFragment.newInstance(args);
+            // Prefer replace() over add() see <a>https://github.com/RowlandOti/PopularMovies/issues/1</a>
+            ft.replace(R.id.detail_container, detailFragment);
+            ft.commit();
+        }
+    }
 }
