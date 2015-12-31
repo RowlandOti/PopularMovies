45668a46c6a3888ecfd35089d31d4df551c3c035
RowlandOti <rowlandotienoo@gmail.com>
2015-12-31 16:35:50 +0300

    feat: add AppBarLayout

--- app/src/main/java/com/rowland/movies/data/repository/MovieRepository.java
+++ app/src/main/java/com/rowland/movies/data/repository/MovieRepository.java
@@ -61,7 +61,8 @@ public class MovieRepository {
             // ToDo: Move this logic to the Movie model where it belongs
             // Query ActiveAndroid for list of data
             List<Movie> queryResults = new Select()
-                    .from(Movie.class).where(whereClause, true)
+                    .from(Movie.class)
+                    .where(whereClause, true)
                     .orderBy("id ASC")
                     .limit(100).execute();
             // This is how you execute a query
