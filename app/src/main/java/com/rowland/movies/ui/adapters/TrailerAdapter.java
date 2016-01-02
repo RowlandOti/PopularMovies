ad484abde72f899b9fc008433f0ad9341bd4787d
RowlandOti <rowlandotienoo@gmail.com>
2016-01-02 13:08:41 +0300

    chore: create debug code

--- app/src/main/java/com/rowland/movies/ui/adapters/TrailerAdapter.java
+++ app/src/main/java/com/rowland/movies/ui/adapters/TrailerAdapter.java
@@ -140,6 +140,10 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.CustomVi
                 }
             });
 
+            // Check wether we are in debug mode
+            if (BuildConfig.IS_DEBUG_MODE) {
+                Log.d(LOG_TAG, "Current Trailer: " + trailer.getName());
+            }
         }
     }
 }
